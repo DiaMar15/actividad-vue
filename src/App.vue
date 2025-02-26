9e4059b7c93bb015d36fef1379f7d06d38cd3fe5
DiaMar15 <diamarcelaleal23@gmail.com>
2025-02-25 20:50:13 -0500

    Actualizacion del App.vue con Not Found

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,17 @@
 import { useRoute } from 'vue-router'
 import AuthLayout from './components/layout/AuthLayout.vue'
 import WireFlameLayout from './components/layout/WireFlameLayout.vue'
+import NotFoundLayout from './components/layout/NotFoundLayout.vue'
 import { computed } from 'vue'
 
 const route = useRoute()
-const layouts = { AuthLayout, WireFlameLayout }
-const layout = computed(() => layouts[route.meta.layout] || AuthLayout)
+
+// Agregar un layout predeterminado para la vista no encontrada
+const layouts = { AuthLayout, WireFlameLayout, NotFoundLayout }
+const layout = computed(() => {
+  // Si la ruta tiene un layout, úsalo, sino usa NotFoundLayout para rutas no encontradas
+  return layouts[route.meta.layout] || layouts['NotFoundLayout']
+})
 </script>
 
 <style scoped>
