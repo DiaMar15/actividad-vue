624cb9dc1ad43d5e274cbd762ec1715cc2bc163b
DiaMar15 <diamarcelaleal23@gmail.com>
2025-02-25 20:46:17 -0500

    El layout Not Found

---
+++ src/components/layout/NotFoundLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="not-found">
+    <router-view />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.not-found {
+  text-align: center;
+  margin-top: 50px;
+}
+.not-found h1 {
+  font-size: 3em;
+  color: #f44336;
+}
+.not-found p {
+  font-size: 1.2em;
+}
+</style>
